b2a4ba38af9601764d5ebbe022246d35aad65744
SuShruthi <shruthacm@gmail.com>
2023-04-22 01:02:26 -0700

    add file size instead of hardcoded, implement all the edge cases

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const request = require('request');
 const fs = require('fs');
+const readline = require('readline');
 
 const url = process.argv[2];
 const localFilePath = process.argv[3];
 
+// Edge case 2: file path is invalid
+if (!fs.existsSync(localFilePath)) {
+  console.error('Invalid file path.');
+  process.exit();
+}
+
+
 request(url, (error, response, body) => {
-  console.log('error:', error);
-  console.log('statusCode:', response && response.statusCode);
-  console.log('body:', body);
+  //Edge case 3: if url is invalid
+  if (error) {
+    console.error(`Error: ${error}`);
+    process.exit(1);
+  }
+  else {
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+    console.log('body:', body);
+  }
 
   fs.writeFile(localFilePath, body, (error) => {
-    console.log(`Downloaded and saved 1235 bytes to ${localFilePath}.`);
+    if (error) {
+      console.error(`Error writing file: ${error}`);
+      process.exit();
+    } else {
+      console.log(`Downloaded and saved ${body.length} bytes to ${localFilePath}.`);
+    }
   });
 
 });
